7999a26a4a1420370d7ec5bf657c5c306893377d
Carla Pena <exoreiba@gmail.com>
2023-12-07 11:33:58 -0300

    listo pagina nosotros

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,30 @@ import express  from "express"; // version import
 const router = express.Router();
 
 router.get('/', (req, res) => { //req - lo q enviamos, res -> lo q express responde
-    res.render('inicio');
+
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
 });
 
 router.get('/nosotros', (req, res) => { //req - lo q enviamos, res -> lo q express responde    
-    res.render('nosotros');
-
-
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    });
 });
 
+
+router.get('/viajes', (req, res) => { //req - lo q enviamos, res -> lo q express responde    
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+
+router.get('/testimoniales', (req, res) => { //req - lo q enviamos, res -> lo q express responde    
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
+});
 
 export default router;
