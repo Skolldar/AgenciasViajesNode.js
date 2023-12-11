8a525aa10dad7149fa22a885551eabea1d302e53
Carla Pena <exoreiba@gmail.com>
2023-12-11 14:46:59 -0300

    added db url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config() 
 
-const db = new Sequelize(process.env.DB_URL, {
+const db = new Sequelize(process.env.DATABASE_URL, {
     define: {
         timestamps: false
     },
